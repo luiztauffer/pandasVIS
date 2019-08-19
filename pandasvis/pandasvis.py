d687c16f3132f584ba350eabad62fc58f44caba0
luiztauffer <luiz_tauffer@hotmail.com>
2019-08-19 15:50:12 +0200

    report on main tab

--- pandasvis/pandasvis.py
+++ pandasvis/pandasvis.py
@@ -2,11 +2,15 @@ from PyQt5 import QtCore, QtGui, Qt
 from PyQt5.QtWidgets import (QWidget, QApplication, QTreeWidget, QTreeWidgetItem,
     QMainWindow, QFileDialog, QAction, QVBoxLayout, QHBoxLayout, QGridLayout,
     QPushButton, QTreeWidgetItemIterator, QTabWidget)
+from PyQt5.QtWebEngineWidgets import QWebEnginePage, QWebEngineView
+
 from console_widget import ConsoleWidget
-import pandas as pd
 import numpy as np
+import pandas as pd
+import pandas_profiling
 import os
 import sys
+import shutil
 
 class Application(QMainWindow):
     def __init__(self, filename):
@@ -14,7 +18,7 @@ class Application(QMainWindow):
 
         self.centralwidget = QWidget()
         self.setCentralWidget(self.centralwidget)
-        self.resize(1000, 600)
+        self.resize(1200, 900)
         self.setWindowTitle('PandasVIS')
 
         #Initialize GUI elements
@@ -50,11 +54,11 @@ class Application(QMainWindow):
         #action_about.triggered.connect(self.about)
 
         # Left panels ----------------------------------------------------------
-        self.tree_primary = QTreeWidget ()
-        self.tree_primary.setHeaderLabels(['Primary'])
+        self.tree_primary = QTreeWidget()
+        self.tree_primary.setHeaderLabels(['Primary Variables'])
         #self.tree_primary.itemClicked.connect(self.onItemClicked)
-        self.tree_secondary = QTreeWidget ()
-        self.tree_secondary.setHeaderLabels(['Secondary'])
+        self.tree_secondary = QTreeWidget()
+        self.tree_secondary.setHeaderLabels(['Secondary Variables'])
         #self.tree_secondary.itemClicked.connect(self.onItemClicked)
 
         self.grid_left1 = QGridLayout()
@@ -65,23 +69,21 @@ class Application(QMainWindow):
         self.tabs = QTabWidget()
         self.tab1 = QWidget()
         self.tab2 = QWidget()
-        self.tabs.resize(600,600)
-        self.tabs.addTab(self.tab1,"Tab 1")
+        #self.tabs.resize(600,600)
+        self.tabs.addTab(self.tab1,"Overview")
         self.tabs.addTab(self.tab2,"Tab 2")
          # Create first tab
-        #self.tab1.layout = QVBoxLayout()
-        #self.pushButton1 = QPushButton("PyQt5 button")
-        #self.tab1.layout.addWidget(self.pushButton1)
-        #self.tab1.setLayout(self.tab1.layout)
+        self.tab1.layout = QVBoxLayout()
+        self.webview = QWebEngineView()
+        self.tab1.layout.addWidget(self.webview)
+        self.tab1.setLayout(self.tab1.layout)
+        #self.refresh_tab1()
 
         self.console = ConsoleWidget(par=self)
 
         self.vbox1 = QVBoxLayout()
         self.vbox1.addWidget(self.tabs)
         self.vbox1.addWidget(self.console)
-
-        #self.vbox2 = QVBoxLayout()
-        #self.vbox2.addWidget(self.console)
 
         self.hbox = QHBoxLayout(self.centralwidget)
         self.hbox.addLayout(self.grid_left1)      #add left panel
@@ -96,37 +98,15 @@ class Application(QMainWindow):
             self.file_path = filename
             self.df = pd.read_csv(self.file_path)
             self.primary_names = self.df.keys().tolist()
+            self.df_profile = self.df.profile_report(title='Summary Report', style={'full_width':True}, )
+            self.temp_dir = os.path.join(os.getcwd(),'temp')
+            if not os.path.exists(self.temp_dir):
+                os.makedirs(self.temp_dir, exist_ok=True)
+            self.df_profile.to_file(os.path.join(self.temp_dir,'summary_report.html'), silent=True)
             self.setWindowTitle('PandasVIS - '+os.path.split(os.path.abspath(self.file_path))[1])
             self.init_trees()
             self.init_console()
-
-    def find_selected_items(self):
-        """Iterate over all children of the tree and save selected items to dictionary."""
-        self.cp_objs = {}
-        self.iterator = QTreeWidgetItemIterator(self.tree, QTreeWidgetItemIterator.All)
-        while self.iterator.value():
-            item = self.iterator.value()
-            if item.checkState(0)==2: #full-box checked, add item to dictionary
-                if item.parent() is not None: #2nd level item (at least)
-                    if item.parent().text(0) in self.cp_objs:  #append if parent already present as key in dictionary
-                        if self.cp_objs[item.parent().text(0)]==True: #remove boolean parent key:value
-                            self.cp_objs.pop(item.parent().text(0), None)
-                            self.cp_objs[item.parent().text(0)] = [item.text(0)]
-                        else:
-                            self.cp_objs[item.parent().text(0)].append(item.text(0))
-                    else: #add new list with item, if parent not yet a key in dictionary
-                        self.cp_objs[item.parent().text(0)] = [item.text(0)]
-                else:  #1st level item
-                    self.cp_objs[item.text(0)] = True
-            self.iterator += 1
-        self.console.push_vars({'cp_objs':self.cp_objs})
-
-    def toggle_auto_clear(self):
-        ''' Toggle auto-clear console screen function'''
-        if self.auto_clear:
-            self.auto_clear = False
-        else:
-            self.auto_clear=True
+            self.refresh_tab1()
 
     def init_trees(self):
         ''' Draw hierarchical tree of fields in NWB file '''
@@ -136,22 +116,20 @@ class Application(QMainWindow):
             parent = QTreeWidgetItem(self.tree_primary)
             parent.setText(0, var1)
             parent.setFlags(parent.flags() | QtCore.Qt.ItemIsTristate | QtCore.Qt.ItemIsUserCheckable)
-            parent.setCheckState(0, QtCore.Qt.Unchecked)
-            #if len(self.nwb.fields[field])==0:
-            #    font = QtGui.QFont()
-            #    font.setWeight(6)
-            #    parent.setFont(0, font)
-
+            parent.setCheckState(0, QtCore.Qt.Checked)
 
     def init_console(self):
         ''' Initialize commands on console '''
-        #self.console._execute("import nwbext_ecog", True)
-        #self.console._execute("from pynwb import NWBHDF5IO", True)
-        #self.console._execute("fname = '"+self.file+"'", True)
-        #self.console._execute("io = NWBHDF5IO(fname,'r+')", True)
-        #self.console._execute("nwb = io.read()", True)
+        self.console._execute("import pandas as pd", True)
+        self.console._execute("import numpy as np", True)
+        self.console._execute("import matplotlib.pyplot as plt", True)
+        self.console.push_vars({'df':self.df})
         self.console.clear()
-        #self.console.execute_command("")
+
+    def refresh_tab1(self):
+        url = QtCore.QUrl.fromLocalFile(os.path.join(self.temp_dir,'summary_report.html'))
+        self.webview.load(url)
+        self.webview.show()
 
     def onItemClicked(self, it, col):
         if self.auto_clear:  #clears terminal
@@ -175,6 +153,31 @@ class Application(QMainWindow):
         self.console.push_vars({'item':item})
         self.console._execute("print(item)", False)
 
+    def find_selected_items(self):
+        """Iterate over all children of the tree and save selected items to dictionary."""
+        self.cp_objs = {}
+        self.iterator = QTreeWidgetItemIterator(self.tree, QTreeWidgetItemIterator.All)
+        while self.iterator.value():
+            item = self.iterator.value()
+            if item.checkState(0)==2: #full-box checked, add item to dictionary
+                if item.parent() is not None: #2nd level item (at least)
+                    if item.parent().text(0) in self.cp_objs:  #append if parent already present as key in dictionary
+                        if self.cp_objs[item.parent().text(0)]==True: #remove boolean parent key:value
+                            self.cp_objs.pop(item.parent().text(0), None)
+                            self.cp_objs[item.parent().text(0)] = [item.text(0)]
+                        else:
+                            self.cp_objs[item.parent().text(0)].append(item.text(0))
+                    else: #add new list with item, if parent not yet a key in dictionary
+                        self.cp_objs[item.parent().text(0)] = [item.text(0)]
+                else:  #1st level item
+                    self.cp_objs[item.text(0)] = True
+            self.iterator += 1
+        self.console.push_vars({'cp_objs':self.cp_objs})
+
+    def closeEvent(self, event):
+        """Before exiting, deletes temporary files."""
+        shutil.rmtree(self.temp_dir, ignore_errors=False, onerror=None)
+        event.accept()
 
 
 
